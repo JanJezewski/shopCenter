30cf29d791f47f79d4d07fb7472cb60a35a8127a
unknown <janjezewski01@gmail.com>
2023-03-10 05:44:11 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
